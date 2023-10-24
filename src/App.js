acab9f8be5bc37fe53f03312c267edc87abbbcd6
mfranke84 <markus.franke84@gmail.com>
2023-10-24 05:39:45 +0200

    Changed app component to class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import './App.css';
+import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 
-
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Rank />
-      <ImageLinkForm />
-      {
-        // <FaceRecognition />
-
-      }
-      <ParticlesBg type="cobweb" bg={true} />
-    </div>
-
-
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Rank />
+        <ImageLinkForm />
+        {
+          // <FaceRecognition />
+  
+        }
+        <ParticlesBg type="cobweb" bg={true} />
+      </div>
+  
+  
+    ); 
+  }
 }
 
 export default App;
